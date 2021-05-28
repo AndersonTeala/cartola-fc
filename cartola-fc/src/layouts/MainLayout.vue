ae1b8cffcbc6f28ae6d210f0ec2c7d86fac89ab0
Anderson Teala <andersonteala@gmail.com>
2021-05-27 21:35:55 -0300

    Create new page

---
+++ cartola-fc/src/layouts/MainLayout.vue
@@ -0,0 +1,33 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar>
+
+        <q-toolbar-title>
+          <router-link to="/">
+            <img src="cartola-fc.png" alt="" style="height:2em;" />
+          </router-link>
+        </q-toolbar-title>
+
+        <div>Cartola-FC v{{ $q.version }}</div>
+      </q-toolbar>
+    </q-header>
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import EssentialLink from 'components/EssentialLink.vue'
+
+export default {
+  name: 'MainLayout',
+  components: { EssentialLink },
+  data () {
+    return {
+      //
+    }
+  }
+}
+</script>
